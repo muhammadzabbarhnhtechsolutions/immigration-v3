39915d0740894b90132cb612087b028dfa13886e
Muhammad Zawwar <muhammadzabbar@hnhtechsolutions.com>
2025-08-30 19:13:04 +0500

    done alls

--- src/components/Home3/HowItWorks.jsx
+++ src/components/Home3/HowItWorks.jsx
@@ -12,7 +12,8 @@ import img5 from "../../assets/ig2.png";
 import img6 from "../../assets/ig3.png";
 import img7 from "../../assets/newUser.jpg";
 import bg from "../../assets/bg33.png";
-// ...
+// ..
+// .
 const HomeSections = () => {
   const [current, setCurrent] = useState(0);
   const startX = useRef(null);
