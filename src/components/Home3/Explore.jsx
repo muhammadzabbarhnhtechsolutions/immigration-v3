046ac61ea1d743addab711f807340c4c54e42be3
Muhammad Zawwar <muhammadzabbar@hnhtechsolutions.com>
2025-09-03 14:43:17 +0500

    done all change in Ui

--- src/components/Home3/Explore.jsx
+++ src/components/Home3/Explore.jsx
@@ -6,6 +6,7 @@ import img3 from "../../assets/pic6.png";
 import logo from "../../assets/logo2.0.png";
 import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 // ...
+// ...
 const ExploreSections = () => {
   return (
     <div className="w-full mt-2 mb-20">
