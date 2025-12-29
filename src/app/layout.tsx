59f8f2b0315366de3aac5cb77eac01fdabba398d
Muhammad Zawwar <muhammadzabbar@hnhtechsolutions.com>
2025-12-29 14:32:54 +0500

    done all fixes and new ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import "./globals.css";
-import { GeistSans, GeistMono } from "geist/font"; // objects
 import localFont from "next/font/local";
 import StoreProvider from "./StoreProvider";
 import { ToastContainer } from "react-toastify";
@@ -11,33 +10,27 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-// Geist fonts are objects, no need to call
-const geistSans = GeistSans; 
-const geistMono = GeistMono;
-
-// Local Poppins
 const poppins = localFont({
   src: [
-    { path: "/fonts/Poppins-Regular.woff2", weight: "400" },
-    { path: "/fonts/Poppins-Medium.woff2", weight: "500" },
-    { path: "/fonts/Poppins-Bold.woff2", weight: "700" },
+    { path: "../fonts/Poppins-Regular.ttf", weight: "400" },
+    { path: "../fonts/Poppins-Medium.ttf", weight: "500" },
+    { path: "../fonts/Poppins-Bold.ttf", weight: "700" },
   ],
   variable: "--font-poppins",
+  display: "swap",
 });
 
-// Local Marko One
 const marko = localFont({
-  src: "/fonts/MarkoOne-Regular.woff2",
+  src: "../fonts/MarkoOne-Regular.ttf",
   weight: "400",
   variable: "--font-marko",
+  display: "swap",
 });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} ${marko.variable} antialiased`}
-      >
+      <body className={`${poppins.variable} ${marko.variable} antialiased`}>
         <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
           <StoreProvider>{children}</StoreProvider>
           <ToastContainer />
